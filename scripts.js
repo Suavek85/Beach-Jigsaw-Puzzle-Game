5d1b73bb87a484393bd4e2df4bf4ba23edb51577
SNiemiec <slawekniemiec@gmail.com>
2019-06-17 14:18:05 +0200

    Adding reverting to not matching draggables

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
 $(function() {
-  $("#draggable").draggable(
+  $(".draggable").draggable(
     { grid: [40, 40] },
     { opacity: 0.5 }, 
   );
-  $("#droppable").droppable({
+  $(".droppable").droppable({
+    drop: function(event, ui) {
+      $(this);
+      var dropped = ui.draggable;
+      var droppedIdentifier = ui.draggable.attr("data-identifier");
+      if (event.target.dataset.identifier === droppedIdentifier) {
+        console.log("success");
+        var droppedOn = $(this);
+      $(dropped)
+        .detach()
+        .css({ top: 0, left: 0 })
+        .appendTo(droppedOn);
+      } else {
+        console.log("wrong");
+        $(dropped)
+        .draggable({ revert: "valid" })
+      }
+    }
+  });
+  /*
+  $(".droppable").droppable({
     drop: function(event, ui) {
       $(this);
       var dropped = ui.draggable;
@@ -20,4 +40,12 @@ $(function() {
       }
     }
   });
+*/
+
+
+
+
+
+
+
 });
