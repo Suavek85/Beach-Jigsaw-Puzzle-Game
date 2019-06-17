b3bc2baaae6221af918eda098b5dbfbbbf2c5b41
SNiemiec <slawekniemiec@gmail.com>
2019-06-17 22:44:18 +0200

    Creating first jigsaw

--- scripts.js
+++ scripts.js
@@ -1,24 +1,30 @@
 $(function() {
-  $(".draggable").draggable(
-    { grid: [40, 40] },
-    { opacity: 0.5 }, 
-  );
+  $(".draggable").draggable({ grid: [40, 40] }, { opacity: 0.5 });
   $(".droppable").droppable({
     drop: function(event, ui) {
       $(this);
       var dropped = ui.draggable;
       var droppedIdentifier = ui.draggable.attr("data-identifier");
       if (event.target.dataset.identifier === droppedIdentifier) {
-        console.log("success");
         var droppedOn = $(this);
-      $(dropped)
-        .detach()
-        .css({ top: 0, left: 0 })
-        .appendTo(droppedOn);
-      } else {
-        console.log("wrong");
         $(dropped)
-        .draggable({ revert: "valid" })
+          .detach()
+          .css({ top: 0, left: 0 })
+          .appendTo(droppedOn);
+
+        $(dropped).draggable("destroy");
+        var el = $(".alert");
+        el.html("Great! Well done.");
+        el.addClass("alert-success");
+        el.removeClass("alert-info");
+        el.removeClass("alert-danger");
+      } else {
+        $(dropped).draggable({ revert: "valid" });
+        var el = $(".alert");
+        el.html("Wrong! Try again.");
+        el.addClass("alert-danger");
+        el.removeClass("alert-info");
+        el.removeClass("alert-success");
       }
     }
   });
@@ -41,11 +47,4 @@ $(function() {
     }
   });
 */
-
-
-
-
-
-
-
 });
